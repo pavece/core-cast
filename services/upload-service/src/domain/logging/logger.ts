37a89974afb516f5559ec72f526cfb08402e05be
Pavece <55211797+pavece@users.noreply.github.com>
2025-06-29 22:36:12 +0200

    feat(us)/add pino logger

---
+++ services/upload-service/src/domain/logging/logger.ts
@@ -0,0 +1,34 @@
+import pino, { BaseLogger } from 'pino';
+import fs from 'fs';
+import path from 'path';
+
+export class Logger {
+	public static _instance: Logger;
+	private logger: BaseLogger | undefined;
+
+	constructor() {
+		if (Logger._instance) {
+			return Logger._instance;
+		}
+
+		const logDir = path.join(__dirname, '../../../logs');
+
+		if (!fs.existsSync(logDir)) {
+			fs.mkdirSync(logDir);
+		}
+
+		const prettyTransport = pino.transport({ target: 'pino-pretty', options: { colorize: true } });
+		const fileTransport = pino.transport({
+			target: 'pino/file',
+			options: { destination: path.join(logDir, 'app.log') },
+		});
+
+		this.logger = pino({}, pino.multistream([prettyTransport, fileTransport]));
+
+		Logger._instance = this;
+	}
+
+	public getLogger() {
+		return this.logger as BaseLogger;
+	}
+}
