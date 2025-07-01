8d7cac8af4e764014d4a2cff57a18792b4ef3b1d
Pavece <55211797+pavece@users.noreply.github.com>
2025-07-01 11:37:07 +0200

    feat(ts)/add rabbitMQ

--- services/transcoding-service/src/app.ts
+++ services/transcoding-service/src/app.ts
@@ -1,7 +1,9 @@
 import { Logger } from './domain/logging/logger';
+import { RabbitMQ } from './infrastructure/rabbitmq/rabbitmq';
 
 function main() {
 	new Logger();
+	new RabbitMQ();
 
 	console.log('Transcoding service');
 }
---
+++ services/transcoding-service/src/infrastructure/rabbitmq/rabbitmq.ts
@@ -0,0 +1,37 @@
+import amqp, { Channel } from 'amqplib';
+import 'dotenv/config';
+import { Logger } from '../../domain/logging/logger';
+import { BaseLogger } from 'pino';
+
+export class RabbitMQ {
+	private static _instance: RabbitMQ;
+	private logger: BaseLogger | undefined;
+
+	private client: amqp.ChannelModel | undefined;
+
+	public videoProcessingChannel: Channel | undefined;
+	public videoProcessingQueueName: string = 'video-processing';
+
+	constructor() {
+		if (RabbitMQ._instance) return RabbitMQ._instance;
+
+		this.logger = new Logger().getLogger();
+
+		this.connect();
+
+		RabbitMQ._instance = this;
+	}
+
+	private async connect() {
+		try {
+			this.client = await amqp.connect(process.env.RABBITMQ_CON_URL || 'amqp://localhost');
+			this.videoProcessingChannel = await this.client.createChannel();
+
+			this.videoProcessingChannel.assertQueue('video-processing', { durable: true });
+
+			this.logger?.info('Connected to RabbitMQ broker');
+		} catch (error) {
+			this.logger?.error({ message: 'Failed to connect to RabbitMQ broker', error });
+		}
+	}
+}
