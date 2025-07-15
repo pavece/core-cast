41914efc4d22d0aa7f8c0e6099c390dbba41edef
Pavece <55211797+pavece@users.noreply.github.com>
2025-07-15 16:49:30 +0200

    feat/add meilisearch

---
+++ shared/meilisearch/index.ts
@@ -0,0 +1,19 @@
+import { MeiliSearch } from 'meilisearch';
+
+export class Meili {
+	private static _instance: Meili;
+
+	public client: MeiliSearch | undefined;
+
+	public static getInstance() {
+		if (!this._instance) {
+			Meili._instance = new Meili();
+		}
+
+		return Meili._instance;
+	}
+
+	public async connect(host: string, apiKey: string) {
+		this.client = new MeiliSearch({ host, apiKey });
+	}
+}
