9baf980e55a3205cbb4e41da0e7e24f5187e83b6
Pavece <55211797+pavece@users.noreply.github.com>
2025-07-17 16:32:09 +0200

    feat/add qdrant + setup client

---
+++ shared/qdrant/index.ts
@@ -0,0 +1,36 @@
+import { QdrantClient } from '@qdrant/js-client-rest';
+
+export class Qdrant {
+	private static _instance: Qdrant;
+	private client: QdrantClient | undefined;
+
+	public static getInstance() {
+		if (!Qdrant._instance) {
+			Qdrant._instance = new Qdrant();
+		}
+
+		return Qdrant._instance;
+	}
+
+	public async connect(url: string) {
+		this.client = new QdrantClient({ url });
+
+		const videosCollection = await this.client.getCollection('videos');
+		if (videosCollection) {
+			console.log("Qdrant collection 'videos' already exists, skiping creation");
+			return;
+		}
+
+		await this.client.createCollection('videos', {
+			vectors: {
+				size: 384, //all-MiniLM-L6-v2
+				distance: 'Cosine',
+			},
+		});
+	}
+
+	public getClient() {
+		if (!this.client) throw new Error('Redis client is not connected');
+		return this.client;
+	}
+}
--- shared/types/dist/index.d.ts
+++ shared/types/dist/index.d.ts
@@ -1,2 +1,3 @@
 export type * as Upload from './upload/upload.types';
+export type * from './search/video-search';
 export type * from './api/index';
