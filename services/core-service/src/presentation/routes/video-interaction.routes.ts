3dc837ffdea28e868345be81cdc5226ebeca3854
Pavece <55211797+pavece@users.noreply.github.com>
2025-07-22 13:27:18 +0200

    feat(cs)/add rate limitting for register view endpoint

--- services/core-service/src/presentation/routes/video-interaction.routes.ts
+++ services/core-service/src/presentation/routes/video-interaction.routes.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 import { VideoInteractionsController } from '../controllers/video-interaction/video-interaction.controller';
 import { validateSession } from '../middlewares/validate-session';
+import rateLimit from 'express-rate-limit';
 
 export class VideoInteractionRouter {
 	public static get routes() {
 		const router = Router();
 		const controller = new VideoInteractionsController();
+		const rateLimitter = rateLimit({ windowMs: 1 * 60 * 1000, limit: 5 });
 
 		router.get('/:videoId', controller.getVideoInteractions);
 		router.get('/personal/:videoId', validateSession, controller.getPersonalVideoInteractions);
 		router.post('/like/:videoId', validateSession, controller.toggleVideoLike);
-		router.put('/register-view/:videoId', controller.viewVideo);
+		router.put('/register-view/:videoId', rateLimitter, controller.viewVideo);
 
 		return router;
 	}
