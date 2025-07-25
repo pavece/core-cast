e2cd5ab7892fef2664f890a02364078604944717
Pavece <55211797+pavece@users.noreply.github.com>
2025-07-25 13:04:57 +0200

    feat(front)/add (basic) login page UI implementation

--- frontend/src/app/(user)/auth/(forms)/login/page.tsx
+++ frontend/src/app/(user)/auth/(forms)/login/page.tsx
@@ -1,13 +1,73 @@
+'use client';
+
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import z from 'zod';
+
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const loginFormSchema = z.object({
+	email: z.email(),
+	password: z.string().min(6),
+});
 
 const LoginPage = () => {
+	const form = useForm<z.infer<typeof loginFormSchema>>({
+		resolver: zodResolver(loginFormSchema),
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	});
+
+	function onSubmit(values: z.infer<typeof loginFormSchema>) {
+		console.log(values);
+	}
+
 	return (
 		<div>
 			<div className='text-center'>
 				<h1 className='text-2xl font-semibold'>Sign In</h1>
 				<p className='text-muted-foreground'>Fill your credentials to access the creator pannel.</p>
 			</div>
-			<div>{/* TODO: Login form */}</div>
+			<div className='mt-6'>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+						<FormField
+							control={form.control}
+							name='email'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input placeholder='john@acme.inc' {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name='password'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input type='password' {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type='submit' className='w-full'>
+							Login
+						</Button>
+					</form>
+				</Form>
+			</div>
 		</div>
 	);
 };
