2ab7250bd1cd32619f9dd236df715ffcef28f4e0
Pavece <55211797+pavece@users.noreply.github.com>
2025-07-27 00:53:59 +0200

    feat(front)/add creator pannel sidebar

--- frontend/src/app/(user)/creator-pannel/layout.tsx
+++ frontend/src/app/(user)/creator-pannel/layout.tsx
@@ -1,5 +1,19 @@
 import { checkSession, closeSession } from '@/api/coreApi';
-import { UserProfile } from '@/components/user/user-profile';
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarProvider,
+} from '@/components/ui/sidebar';
+import { SidebarUserProfile } from '@/components/user/sidebar-user-profile';
+import { LayoutList, Plus, Settings } from 'lucide-react';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import React from 'react';
@@ -18,13 +32,51 @@ export default async function CreatorPannelLayout({
 		if (!validationResult.data.user.username) return redirect('/');
 
 		return (
-			<main className='p-4'>
-				<nav className='flex justify-between'>
-					<img src='/logo.svg' alt='Core cast logo' className='w-[140px] md:w-[160px]' />
-					<UserProfile username={validationResult.data.user.username} />
-				</nav>
-				{children}
-			</main>
+			<SidebarProvider>
+				<Sidebar variant='floating'>
+					<SidebarHeader className='flex items-center'>
+						<img src='/logo.svg' alt='Core cast logo' className='w-[140px] md:w-[160px]' />
+					</SidebarHeader>
+					<SidebarContent>
+						<SidebarGroup>
+							<SidebarGroupLabel>Channel & User</SidebarGroupLabel>
+							<SidebarGroupContent>
+								<SidebarMenu>
+									<SidebarMenuItem>
+										<SidebarMenuButton className='cursor-pointer'>
+											<Settings /> Settings
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+								</SidebarMenu>
+							</SidebarGroupContent>
+						</SidebarGroup>
+						<SidebarGroup>
+							<SidebarGroupLabel>Videos</SidebarGroupLabel>
+							<SidebarGroupContent>
+								<SidebarMenu>
+									<SidebarMenuItem>
+										<SidebarMenuButton className='cursor-pointer'>
+											<Plus /> Create video
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+									<SidebarMenuItem>
+										<SidebarMenuButton className='cursor-pointer'>
+											<LayoutList /> Manage videos
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+								</SidebarMenu>
+							</SidebarGroupContent>
+						</SidebarGroup>
+					</SidebarContent>
+
+					<SidebarFooter>
+						<SidebarUserProfile username={validationResult.data.user.username} role={validationResult.data.user.role} />
+					</SidebarFooter>
+				</Sidebar>
+				<main className='p-4'>
+					<div>{children}</div>
+				</main>
+			</SidebarProvider>
 		);
 	} catch {
 		return redirect('/');
---
+++ frontend/src/components/user/sidebar-user-profile.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import React from 'react';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { EllipsisVertical, User } from 'lucide-react';
+import { closeSession } from '@/api/coreApi';
+import { useRouter } from 'next/navigation';
+
+type Props = {
+	username: string;
+	role: string;
+};
+
+export const SidebarUserProfile = ({ username, role }: Props) => {
+	const router = useRouter();
+
+	const onCloseSession = async () => {
+		await closeSession();
+		window.sessionStorage.clear();
+
+		router.push('/');
+	};
+
+	return (
+		<div>
+			<DropdownMenu>
+				<DropdownMenuTrigger className='cursor-pointer w-full'>
+					<div className='flex justify-between w-full'>
+						<div className='flex items-center gap-2'>
+							<Avatar className='rounded-md w-11 h-11'>
+								<AvatarImage src={``} className='rounded-none'></AvatarImage>
+								<AvatarFallback className='rounded-none'>{username.slice(0, 2).toUpperCase()}</AvatarFallback>
+							</Avatar>
+							<div className='text-start'>
+								<h4 className='text-md font-medium'>{username}</h4>
+								<p className='text-sm text-muted-foreground capitalize'>{role.toLowerCase()}</p>
+							</div>
+						</div>
+						<div className='flex items-center justify-center'>
+							<EllipsisVertical />
+						</div>
+					</div>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className='mb-2'>
+					<DropdownMenuLabel className='flex items-center gap-1'>
+						<User className='w-4' /> Account
+					</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<Link href='/auth/settings'>Settings</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem
+						className='cursor-pointer text-red-400 hover:text-red-500 hover:bg-red-400/25'
+						onClick={onCloseSession}
+					>
+						Logout
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
+	);
+};
